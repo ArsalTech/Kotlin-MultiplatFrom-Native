3ccf984a2706fa8f18876e5e04a60a2d0b2ded95
ArsalTech <imarsal777@gmail.com>
2025-03-13 16:11:12 +0500

    Initial commit

--- shared/src/androidMain/kotlin/com/devgate/fusionk/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/devgate/fusionk/Platform.android.kt
@@ -1,7 +1,7 @@
 package com.devgate.fusionk
 
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+    override val name: String = "K ${android.os.Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/iosMain/kotlin/com/devgate/fusionk/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/devgate/fusionk/Platform.ios.kt
@@ -1,9 +1,8 @@
 package com.devgate.fusionk
 
-import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+    override val name: String = "iOS 17"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
